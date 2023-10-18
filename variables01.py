f6c69260ce3df2e0d21f117f53c5ebcfab3a0d7a
az <93015718+FoolishBishop@users.noreply.github.com>
2023-10-17 21:53:41 -0300

    como funcan variables

---
+++ variables01.py
@@ -0,0 +1,51 @@
+# NOTA IMPORTANTE: lo que tiene al inicio # son comentairios, por lo que no ejecutan nada
+
+
+#    ********* VARIABLES *********
+# las variables son donde guardamos informacion, imaginalo como una maleta, cada vez que llamamos a la
+# variable, entonces se mostrara el valor que contiene⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠿⠉⠉⠉⠉⠿⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⣠⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣄⠀⠀⠀⠀
+# ⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀
+# ⠀⠀⠀⠀⡀⠹⣿⣿⣿⣿⣿ [valor]  ⣿⣿⣿⣿⣿⣿⠏⢀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⣿⣆⠘⢿⡿⠿⠿⢿⣿⣿⣿⣿⣿⣿⡿⠿⠿⣿⡿⠃⣰⣿⠀⠀⠀⠀
+# ⠀⠀⠀⠀⣿⣿⣧⣈⡁⢰⡆⢈⣉⣉⣉⣉⣉⣉⡀⣶⠀⣉⣁⣼⣿⣿⠀⠀⠀⠀
+# ⠀⠀⠀⠀⣿⣿⣿⣿⣇⣈⣁⣸⣿⣿⣿⣿⣿⣿⣇⣉⣀⣿⣿⣿⣿⣿⠀⠀⠀⠀
+# ⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠋⠀⠀⠀⠀
+#   ** EJEMPLO**
+# 1)
+# primero, definimos la variable. Estas pueden tener cualquier nombre con tal que no tengan espacios
+variable = "Hola"
+# ^^^ notese que se puso comillas entre el valor, esto porque los valores de texto se les debe  poner comillas
+# la accion "print()" consiste en mostrar en la consola (tu pantalla), en este caso la variable llamada variable
+print(variable)
+# >>> Hola
+
+# 2)
+# las variables no son constantes, por lo que se pueden actualizar
+cantidad_de_manzanas = 46  # <--- aqui no usa comillas porque son valores numericos que podemos sumar, restar, etc
+cantidad_de_manzanas = 21
+# ^^^ volvimos a asignar un valor nuevo a la variable. Tambien podemos poder otro tipo de valor, como texto
+print(cantidad_de_manzanas)
+# >>> 21
+
+# 3)
+# Se le puede asignar valores de otras variable a las variables
+casa_encendida = True  # <-- este es el tipo de valor VERDADERO/FALSO (el que mas vamos a usar)
+luz_encendida = casa_encendida  # <-- le asigno el valor que tiene casa_encendida a luz_encendida
+print(luz_encendida)
+# >>> True
+
+print("----\n EJERCICIOS")  # <--- ignorar
+# ****** EJERCICOS ******
+# 1)
+print("1)")  # <--- ignorar
+# escribe tu nombre en la pantalla:
+
+
+# 2)
+print("2)")  # <--- ignorar
+# asigna un valor a una variable llamada "dias_sin_sol" con un numero y muestrala en la consola
+# tu respuesta:
